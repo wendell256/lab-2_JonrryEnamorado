7fbb6278aeb446893bcde4a3884507f2bde04b9a
Jonrry <jonrry.enamorado@unitec.edu>
2018-05-04 13:44:05 -0600

    Cree el menu

--- Lab#2_JonrryEnamorado/src/lab/pkg2_jonrryenamorado/Lab2_JonrryEnamorado.java
+++ Lab#2_JonrryEnamorado/src/lab/pkg2_jonrryenamorado/Lab2_JonrryEnamorado.java
@@ -4,19 +4,33 @@
  * and open the template in the editor.
  */
 package lab.pkg2_jonrryenamorado;
-
+import java.util.Scanner;
 /**
  *
  * @author wende
  */
 public class Lab2_JonrryEnamorado {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola");
+        Scanner read = new Scanner(System.in);
+        System.out.println("UNIVERSIDAD TECNOLÓGICA");
+        System.out.println("MENÚ PRINCIPAL");
+        System.out.println("1) Administración");
+        System.out.println("2) Crear Cuenta");
+        System.out.println("3) Login");
+        System.out.println("Seleccione una opcion: ");
+        int opc = read.nextInt();
+        switch(opc){
+            case 1: 
+                
+                break;
+                
+                
+        }
     }
     
 }
