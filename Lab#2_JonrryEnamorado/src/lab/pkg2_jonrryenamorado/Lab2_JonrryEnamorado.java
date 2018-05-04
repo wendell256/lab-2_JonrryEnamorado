24387a241c647038dd3cd6c27aa32cbf2cc62a1d
Jonrry <jonrry.enamorado@unitec.edu>
2018-05-04 13:27:00 -0600

    Primer Commit

---
+++ Lab#2_JonrryEnamorado/src/lab/pkg2_jonrryenamorado/Lab2_JonrryEnamorado.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.pkg2_jonrryenamorado;
+
+/**
+ *
+ * @author wende
+ */
+public class Lab2_JonrryEnamorado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola");
+    }
+    
+}
